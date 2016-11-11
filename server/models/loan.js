5ea17973c9aafcbb3ee0afee54e9eef63acfe454
Richard Liu <richardliu7896@gmail.com>
2016-11-10 21:32:59 -0500

    login somewhat working

--- server/models/loan.js
+++ server/models/loan.js
@@ -13,7 +13,6 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Loan.hasMany(models.Users);
         Loan.belongsTo(models.Users, {foreignKey: 'Lender'});
         Loan.belongsTo(models.Users, {foreignKey: 'Receiver'});
       }
