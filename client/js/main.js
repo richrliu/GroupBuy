1f1985b006c0a587e129ac000f748cd170334325
Jonathan Ma <jonathan.ma@duke.edu>
2016-12-13 15:57:19 -0500

    completed messages styling

--- client/js/main.js
+++ client/js/main.js
@@ -2,4 +2,16 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+
+  var chatDiv = document.getElementsByClassName('chat-body')[0]; //I assume you only have one chat box!
+  chatDiv.scrollTop = chatDiv.scrollHeight;
 });
+
+function textAreaAdjust(o) {
+    o.style.height = "1px";
+    if (o.scrollHeight > 150) {
+        o.style.height = "150px";
+    } else {
+        o.style.height = (o.scrollHeight)+"px";
+    }
+}
